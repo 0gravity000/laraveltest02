5cf191cbdb1e2b52c31241b3797994a215c5666e
0gravity000 <0gravity000@gmail.com>
2017-09-02 21:49:37 +0900

    Finish episode 11

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostsController extends Controller
 {
@@ -16,5 +17,33 @@ class PostsController extends Controller
       return view('posts.show');
     }
 
+    public function create()
+    {
+      return view('posts.create');
+    }
+
+    public function store()
+    {
+      //dd(request()->all());
+      //Create a new post using the request data
+//      $post = new Post;
+//      $post->title = request('title');
+//      $post->body = request('body');
+
+      //Save it to the database
+//      $post->save();
+
+      Post::create(request(['title', 'body']));
+
+//      Post::create([
+//        'title' => request('title'),
+//        'body' => request('body'),
+//      ]);
+
+      //And then redirect to the home page.
+
+      return redirect('/');
+
+    }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,23 @@ use App\Task;
 
 Route::get('/','PostsController@index');
 
-Route::get('/posts/{post}','PostsController@show');
+Route::get('/posts/create','PostsController@create');
+
+Route::post('/posts','PostsController@store');
+
+/*
+posts
+
+GET /posts
+GET /posts/create
+POST /posts
+GET /posts/{id}/edit
+GET /posts/{id}
+PATCH /posts/{id}
+DELETE /posts/{id}
+*/
+
+//Route::get('/posts/{post}','PostsController@show');
 
 
 // Controller => PostsController
